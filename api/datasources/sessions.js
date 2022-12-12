b8c79b887cbf683d44a610ff57e1472461d67205
markkong30 <markkong30@gmail.com>
2022-12-12 16:11:59 +0000

    modify add session schema

--- api/datasources/sessions.js
+++ api/datasources/sessions.js
@@ -1,33 +1,33 @@
-const { DataSource } = require("apollo-datasource");
-const lodashId = require("lodash-id");
+const { DataSource } = require('apollo-datasource');
+const lodashId = require('lodash-id');
 
-const low = require("lowdb");
-const FileSync = require("lowdb/adapters/FileSync");
+const low = require('lowdb');
+const FileSync = require('lowdb/adapters/FileSync');
 
-const adapter = new FileSync("./data/sessions.json");
+const adapter = new FileSync('./data/sessions.json');
 const db = low(adapter);
 db._.mixin(lodashId);
 
 class SessionDataSource extends DataSource {
-  constructor() {
-    super();
-  }
+	constructor() {
+		super();
+	}
 
-  initialize(config) {
-    this.db = db.get("sessions");
-  }
+	initialize(config) {
+		this.db = db.get('sessions');
+	}
 
-  getSessions(args) {
-    return this.db.filter(args).value();
-  }
+	getSessions(args) {
+		return this.db.filter(args).value();
+	}
 
-  getSessionById(id) {
-    return this.db.getById(id).value();
-  }
+	getSessionById(id) {
+		return this.db.getById(id).value();
+	}
 
-  createSession(session) {
-    return this.db.insert(session).write();
-  }
+	createSession(session) {
+		return this.db.insert(session).write();
+	}
 }
 
 module.exports = SessionDataSource;
--- api/resolvers/mutation.js
+++ api/resolvers/mutation.js
@@ -1,109 +1,120 @@
-const authUtils = require("../utils/auth");
+const authUtils = require('../utils/auth');
 
 module.exports = {
-  createSession: async (parent, args, { dataSources }, info) => {
-    const session = await dataSources.sessionDataSource.createSession(
-      args.session
-    );
-    return session;
-  },
-  signUp: async (parent, { credentials }, { dataSources, res }, info) => {
-    const { email, password } = credentials;
-    const userCredentials = { email: email.toLowerCase(), password };
+	createSession: async (parent, args, { dataSources }, info) => {
+		const speaker = await dataSources.speakerDataSource.getSpeakerById(
+			args.session.speaker
+		);
 
-    const existingUser = dataSources.userDataSource.getUserByEmail(
-      userCredentials.email
-    );
+		const session = await dataSources.sessionDataSource.createSession({
+			...args.session,
+			speakers: [
+				{
+					id: speaker.id,
+					name: speaker.name
+				}
+			]
+		});
+		return session;
+	},
+	signUp: async (parent, { credentials }, { dataSources, res }, info) => {
+		const { email, password } = credentials;
+		const userCredentials = { email: email.toLowerCase(), password };
 
-    if (existingUser) {
-      throw new Error("A user account with that email already exists.");
-    }
+		const existingUser = dataSources.userDataSource.getUserByEmail(
+			userCredentials.email
+		);
 
-    const hash = authUtils.hashPassword(userCredentials.password);
+		if (existingUser) {
+			throw new Error('A user account with that email already exists.');
+		}
 
-    const dbUser = dataSources.userDataSource.createUser({
-      email: userCredentials.email,
-      hash,
-    });
+		const hash = authUtils.hashPassword(userCredentials.password);
 
-    const token = authUtils.createToken(dbUser);
+		const dbUser = dataSources.userDataSource.createUser({
+			email: userCredentials.email,
+			hash
+		});
 
-    res.cookie("token", token, {
-      httpOnly: true,
-    });
+		const token = authUtils.createToken(dbUser);
 
-    return {
-      user: {
-        id: dbUser.id,
-        email: dbUser.email,
-      },
-    };
-  },
-  signIn: async (parent, { credentials }, { dataSources, res }, info) => {
-    const { email, password } = credentials;
-    const userCredentials = { email: email.toLowerCase(), password };
+		res.cookie('token', token, {
+			httpOnly: true
+		});
 
-    const existingUser = dataSources.userDataSource.getUserByEmail(
-      userCredentials.email
-    );
+		return {
+			user: {
+				id: dbUser.id,
+				email: dbUser.email
+			}
+		};
+	},
+	signIn: async (parent, { credentials }, { dataSources, res }, info) => {
+		const { email, password } = credentials;
+		const userCredentials = { email: email.toLowerCase(), password };
 
-    if (!existingUser) {
-      throw new Error("Incorrect email address or password.");
-    }
+		const existingUser = dataSources.userDataSource.getUserByEmail(
+			userCredentials.email
+		);
 
-    const isValidPassword = authUtils.verifyPassword(
-      password,
-      existingUser.hash
-    );
+		if (!existingUser) {
+			throw new Error('Incorrect email address or password.');
+		}
 
-    if (!isValidPassword) {
-      throw new Error("Incorrect email address or password.");
-    }
+		const isValidPassword = authUtils.verifyPassword(
+			password,
+			existingUser.hash
+		);
 
-    const token = authUtils.createToken(existingUser);
+		if (!isValidPassword) {
+			throw new Error('Incorrect email address or password.');
+		}
 
-    res.cookie("token", token, {
-      httpOnly: true,
-    });
+		const token = authUtils.createToken(existingUser);
 
-    return {
-      user: {
-        id: existingUser.id,
-        email: existingUser.email,
-      },
-    };
-  },
-  userInfo: async (parent, args, { dataSources, user }, info) => {
-    if (user) {
-      return {
-        user: { id: user.sub, email: user.email },
-      };
-    }
+		res.cookie('token', token, {
+			httpOnly: true
+		});
 
-    return {
-      user: undefined,
-    };
-  },
-  signOut: async (parent, args, { dataSources, res }, info) => {
-    res.clearCookie("token");
-    return {
-      user: undefined,
-    };
-  },
-  toggleFavoriteSession: async (parent, args, context, info) => {
-    if (context.user) {
-      const user = await context.dataSources.userDataSource.toggleFavoriteSession(
-        args.sessionId,
-        context.user.sub
-      );
-      return user;
-    }
-    return undefined;
-  },
-  markFeatured: async (parent, args, { dataSources }, info) => {
-    return dataSources.speakerDataSource.markFeatured(
-      args.speakerId,
-      args.featured
-    );
-  },
+		return {
+			user: {
+				id: existingUser.id,
+				email: existingUser.email
+			}
+		};
+	},
+	userInfo: async (parent, args, { dataSources, user }, info) => {
+		if (user) {
+			return {
+				user: { id: user.sub, email: user.email }
+			};
+		}
+
+		return {
+			user: undefined
+		};
+	},
+	signOut: async (parent, args, { dataSources, res }, info) => {
+		res.clearCookie('token');
+		return {
+			user: undefined
+		};
+	},
+	toggleFavoriteSession: async (parent, args, context, info) => {
+		if (context.user) {
+			const user =
+				await context.dataSources.userDataSource.toggleFavoriteSession(
+					args.sessionId,
+					context.user.sub
+				);
+			return user;
+		}
+		return undefined;
+	},
+	markFeatured: async (parent, args, { dataSources }, info) => {
+		return dataSources.speakerDataSource.markFeatured(
+			args.speakerId,
+			args.featured
+		);
+	}
 };
